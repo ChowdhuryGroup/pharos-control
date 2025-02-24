ff3e0e6a8186ec9254091f3be7123b995fe90da4
liam <clink.6@osu.edu>
2025-02-24 15:35:30 -0500

    Added docstrings and formatted

--- pharos.py
+++ pharos.py
@@ -1,38 +1,61 @@
-'''
+"""
 Utility functions for controlling the PHAROS using the REST Server in the
 PHAROS User Application
-'''
+"""
+
 import requests
 
-base_url = "http://localhost:20020/v1/"
-headers = {'Content-Type': 'application/json; charset=utf-8'}
+BASE_URL = "http://localhost:20020/v1/"
+headers = {"Content-Type": "application/json; charset=utf-8"}
 
-def get_pp():
-    response = requests.get(base_url+'Basic/TargetPpDivider')
+
+def get_pp() -> int:
+    """
+    Get the pulse picker divider value
+    """
+    response = requests.get(BASE_URL + "Basic/TargetPpDivider", timeout=10)
     if response.status_code == 200:
         print(f"PP Divider is set to {response.text}")
         return int(response.text)
 
+    # If -1, that means the pulse picker setting wasn't retrieved
+    return -1
+
+
 def set_pp(pp: int):
-    response = requests.put(base_url+'Basic/TargetPpDivider', data=str(pp), headers=headers)
+    """
+    Set the pulse picker divider value
+    """
+    response = requests.put(
+        BASE_URL + "Basic/TargetPpDivider", data=str(pp), headers=headers, timeout=10
+    )
     if response.status_code == 200:
         print(f"PP Divider successfully changed to {pp}")
     elif response.status_code == 400:
         print("Data is formatted incorrectly")
 
+
 def close_shutter():
-    response = requests.post(base_url+"Basic/CloseOutput")
+    """
+    Close the laser shutter
+    """
+    response = requests.post(BASE_URL + "Basic/CloseOutput", timeout=10)
     if response.status_code == 200:
         print("Closing the shutter.")
     elif response.status_code == 403:
         print("Shutter is already closed, not closing.")
 
+
 def open_shutter():
-    response = requests.post(base_url+"Basic/EnableOutput")
+    """
+    Open the laser shutter
+    """
+    response = requests.post(BASE_URL + "Basic/EnableOutput", timeout=10)
     if response.status_code == 200:
         print("Opening the shutter.")
     elif response.status_code == 403:
         print("Shutter is already open, not opening.")
+
 
 if __name__ == "__main__":
     get_pp()
